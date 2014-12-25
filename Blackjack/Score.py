e815f1b219e9203d111b07600fd33666d4a61183
TonyVH <tonyvandenhoek@gmail.com>
2014-12-25 17:30:41 -0500

    edited 12/25/14

--- Blackjack/Score.py
+++ Blackjack/Score.py
@@ -1,44 +1,43 @@
 # Score.py
 
 class Score:
+    """
+        This class contains various functions to calculate scores,
+        and determine the winner of a blackjack game.
+    """
 
-    def evaluateHand(hand):
-        cardValue = []
+    def determineScore(self, hand):
+        "Takes an int array as an arguement, returns the blackjack value of the hand"
         score = 0
         aces = 0
         for c in hand:
-            cardValue.append(int(c[:-1]))
-        for v in cardValue:
-            if v == 1:
+            if c == 1:
                 aces += 1
-            elif v > 10:
-                v = 10
-            score += v
+            elif c > 10:
+                c = 10
+            score += c
         if 21 >= (score+10) and aces > 0:
             score += 10
             aces -= 1
         return score
 
-    def ai_turn(hand):
-        score = evaluateHand(hand)
-        return score < 17
-
-    def playerWins(p, a):
-        return p <= 21 and p > a or p <= 21 and a > 21
-
-    def draw(p, a):
-        return p == a and p <= 21
-
-    def bothBusted(p, a):
-        return p > 21 and a > 21
-
-    def check_win(player_score, ai_score):
-        # Checks who won based on player and ai scores, then returns a string to declare the winner
-        if playerWins(player_score, ai_score):
+    def determineWinner(self, p_score, ai_score):
+        "Checks who won based on player and ai scores, then returns a string to declare the winner"
+        if self._playerWins(p_score, ai_score):
             return "You Win!"
-        elif draw(player_score, ai_score):
+        elif self._draw(p_score, ai_score):
             return "Draw"
-        elif bothBusted(player_score, ai_score):
+        elif self._bothBusted(p_score, ai_score):
             return "You both busted!"
         else:
             return "Computer Wins!"
+
+    def _playerWins(self, p, a):
+        return 21 >= p > a or p <= 21 < a
+
+    def _draw(self, p, a):
+        return 21 >= p == a
+
+    def _bothBusted(self, p, a):
+        return p > 21 < a
+
