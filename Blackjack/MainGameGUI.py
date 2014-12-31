313748db5e225c38a057841ec1590719ff866c32
TonyVH <tonyvandenhoek@gmail.com>
2014-12-30 20:28:08 -0500

    edited 12/30/14

--- Blackjack/MainGameGUI.py
+++ Blackjack/MainGameGUI.py
@@ -8,6 +8,7 @@ from Deck import Deck
 from Button import Button
 from Score import Score
 from Player import Player
+from Blackjack import Blackjack
 
 
 def printCardToScreen(x, y, card):
@@ -45,7 +46,6 @@ while not buttons[5].clicked(p):
 
     # Shuffle
     if buttons[0].clicked(p):
-        deck.shuffle()
         printCardToScreen(8,5.75,"rbv").draw(win)
         p = win.getMouse()
 
@@ -53,41 +53,24 @@ while not buttons[5].clicked(p):
     # Deal Hand (deal two cards to the player and ai)
     elif buttons[1].clicked(p):
         # Deal player hand
-        player_hand.append(deck.deal_one())
-        card(anchor1[0], player_hand[0]).draw(win)
-        player_hand.append(deck.deal_one())
-        card(anchor1[1], player_hand[1]).draw(win)
-        # Deal ai, add 'cover' card hide first card
-        ai_hand.append(deck.deal_one())
         cover = card(anchor2[0], "rbv")
-        card(anchor2[0], ai_hand[0]).draw(win)
         cover.draw(win)
-        ai_hand.append(deck.deal_one())
-        card(anchor2[1], ai_hand[1]).draw(win)
         p = win.getMouse()
 
 
     # Hit (draw a card)
     elif buttons[2].clicked(p):
-        player_hand.append(deck.deal_one())
-        card(anchor1[len(player_hand)-1], player_hand[-1]).draw(win)
         p = win.getMouse()
 
 
     # Stay (ai's hand is assessed; cards are drawn if necessary)
     elif buttons[3].clicked(p):
-        while ai_turn(ai_hand):
-            ai_hand.append(deck.deal_one())
-            card(anchor2[len(ai_hand)-1], ai_hand[-1]).draw(win)
         p = win.getMouse()
 
 
     # Reveal (determine the winner)
     elif buttons[4].clicked(p):
         cover.undraw()
-        player_score = evaluateHand(player_hand)
-        ai_score = evaluateHand(ai_hand)
-        winner = check_win(player_score, ai_score)
         text = Text(Point(5,5.75), winner)
         text.setSize(24)
         text.setStyle("bold")
@@ -100,16 +83,6 @@ while not buttons[5].clicked(p):
         p = win.getMouse()
 # Close the window once 'Quit' has been pressed.
 win.close()
-
-
-def main():
-    # Create a graphics window, set coordinates, set the background image, and run the game loop
-    win = GraphWin("Cards", 1280, 915)
-    win.setCoords(0, 0, 10, 10)
-    center = Point(5, 5)
-    background = Image(center, "background.png")
-    background.draw(win)
-    game_loop(win)
 
 
 if __name__ == '__main__':
